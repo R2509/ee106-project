836bd73a38ca64a705caff758bbb7075730ee7b0
james robertson <88888340+jamesr07@users.noreply.github.com>
2024-01-25 12:29:57 +0000

    new file:   projectStart.py

---
+++ projectStart.py
@@ -0,0 +1,20 @@
+'''
+AUTHOR(S):
+    james Robertson
+    rison padayachy
+
+THIS A TEST UPLOAD TO TEST THAT I CAN COMMIT NEW FILES/CODE
+
+
+
+
+
+
+
+
+ /home/james/anaconda3/envs/python312
+
+'''
+import pandas as pd; import numpy as np; import tkinter as tk; import threading
+##inFile = pd.read_csv("example.csv")
+

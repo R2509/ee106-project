0afab3b70d4c46105f90e18c12c0fdcde362f61e
james robertson <jamessemail00@gmail.com>
2024-01-26 11:28:42 +0000

    ....

---
+++ projectStart.py
@@ -0,0 +1,34 @@
+'''k
+'''
+
+from time import sleep 
+import multiprocessing as mp
+from pathlib import Path
+import pandas as pd
+
+
+
+
+dir_path = Path(__file__).parent.resolve()
+
+pd.read_csv(dir_path / 'sensor_timeseries.csv')
+
+def test():
+    for loop in range (100):
+        sleep(0.1)
+        print('hello')
+  
+def test2():
+    for loop in range(100):
+        sleep(0.1)
+        print("ass")
+
+
+pr1 = mp.Process(target=test)
+pr2 = mp.Process(target=test2)
+pr2.start()
+pr1.start()
+pr2.join()
+pr1.join()
+
+print("th")

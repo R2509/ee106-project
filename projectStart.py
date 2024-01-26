ec946dc13c10c01ffbf56af10f863427917f3a68
R2509 <rich.seychelles@hotmail.com>
2024-01-26 10:13:05 +0000

    added to project configuration

--- projectStart.py
+++ projectStart.py
@@ -15,6 +15,11 @@ THIS A TEST UPLOAD TO TEST THAT I CAN COMMIT NEW FILES/CODE
  /home/james/anaconda3/envs/python312
 
 '''
-import pandas as pd; import numpy as np; import tkinter as tk; import threading
-##inFile = pd.read_csv("example.csv")
+import pandas as pd;
+
+from pathlib import Path
+
+dir_path = Path(__file__).parent.resolve()
+
+pd.read_csv(dir_path / 'sensor_timeseries.csv')
 

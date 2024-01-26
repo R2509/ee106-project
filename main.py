22f5ea72d2024d2fb1aa183b470bedbea61aa64f
R2509 <rich.seychelles@hotmail.com>
2024-01-26 11:24:41 +0000

    testing things

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pathlib import Path
 
 dir_path = Path(__file__).parent.resolve()
-
+print('created path')
 data = pd.read_csv(dir_path / 'sensor_timeseries.csv')
 
 print(data.mean())

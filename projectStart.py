633a35f7f9c9ea66de926e878c6fb0bf90e653d7
R2509 <rich.seychelles@hotmail.com>
2024-02-09 09:08:56 +0000

    fixed merging issues

--- projectStart.py
+++ projectStart.py
@@ -1,77 +1,13 @@
 '''
-PARRALELL PROCESSIN' ENGINE
-what the actual fuck is this meant to do 
-
-
-'''
-
-from time import sleep 
-import multiprocessing as mp
-from pathlib import Path
-import pandas as pd
-import tkinter as tk
-import numpy as numpy
-
-
-
-
-
-dir_path = Path(__file__).parent.resolve()
-
-pd.read_csv(dir_path / 'sensor_timeseries.csv')
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-def test():
-    for loop in range (100):
-        sleep(0.1)
-        print('hello')
-  
-def test2():
-    for loop in range(100):
-        sleep(0.1)
-        print("ass")
-
-
-pr1 = mp.Process(target=test)
-pr2 = mp.Process(target=test2)
-pr2.start()
-pr1.start()
-pr2.join()
-pr1.join()
-
-print("th")
-'''
-
-
-
-
-
-## addition of code from 2feb24(friday lab)
-
-'''
 PARRALLEL PROCESSING ENGINE
 '''
-'''
+
 import multiprocessing as mp
 from pathlib import Path
 import pandas as pd
 import tkinter as tk
 import numpy as numpy
-
-
-
+from time import sleep
 
 
 dir_path = Path(__file__).parent.resolve()
@@ -121,6 +57,4 @@ print("th")
 
 
 def create_process_task():
-    if 
-
-'''
+    if True: pass # REPLACE THIS WITH WHATEVER

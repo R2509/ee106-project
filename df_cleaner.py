115550150774aa4df21c2b2532493b4c0fddb8fc
= <=>
2024-03-21 09:17:43 +0000

    DF cleaner working again

--- df_cleaner.py
+++ df_cleaner.py
@@ -4,47 +4,39 @@
 
 # where final col in df is not "NORMAL"
 
-# use multiprocessing perhaps
 
-from tarfile import data_filter
+
 import pandas
 import multiprocessing as mp
+# Import relevant 
 
 dfrm = pandas.read_csv("sensor_timeseries.csv")
 err_rows = []
 
-dfrm.drop([1])
-print(dfrm)
 
 
 
-
-def clean_dataframe(data_frame_in):
+def clean_dataframe(data_frame_in: pandas.DataFrame):
     err_prev = False
-    for rows in range(0, len(data_frame_in)):
+    for row_index in range(0, len(data_frame_in)):
         # if col last != normal then remove it
 
-        if data_frame_in.iat[rows, 54] != "NORMAL":
+        if data_frame_in.at[row_index, 'machine_status'] != "NORMAL":
+            # if the dataframe has ann error or recovering state at this particilar row do below items
+            err_rows.append(row_index)
+            # Append rows with errors to list of eror rows
             
-            
+    data_frame_out = data_frame_in.drop(err_rows)
+    print("there was", str(len(err_rows)), "rows containing errors\n    all removed successfully")
+   # print(data_frame_out)
 
-
-        err_rows.append(rows)
-           # print(" ERROR FOUND AT LINE", rows)
     #print(err_rows)
-    data_frame_in = data_frame_in.drop(err_rows)
+    return data_frame_out
     
     
-    for rows in range(0, len(data_frame_in)):
-        # if col last != normal then remove it
-
-        if data_frame_in.iat[rows, 54] != "NORMAL":
-            
-            print(" ERROR FOUND AT LINE", rows, "row not deleted")
     
-        #print(data_frame_in.iat[rows, 54],rows)
 
 
-clean_dataframe(dfrm)
+_ =(clean_dataframe(dfrm))
 
 #\x1b[2J\x1b[A
